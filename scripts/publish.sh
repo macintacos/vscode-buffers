57007e2fdaab26eef6a87bfea0088c9857546b92
Julian Torres <julian@excessive.dev>
2025-03-25 19:47:04 -0400

    fix(build): adjust the changelog generated when running release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 version_to_bump="$(pnpm exec git-cliff --bumped-version)"
 
-pnpm exec git-cliff --bump
+pnpm exec git-cliff --unreleased --tag "$version_to_bump"
 gum confirm "About to publish v$version_to_bump with the above changelog. Proceed?"
 
 # Prepare the changelog, update package.json
 pnpm exec git-cliff --bump -o CHANGELOG.md
+git add . && git commit -m "prerelease: generate changelog for v$version_to_bump"
 pnpm version "$version_to_bump"
-git add . && git commit -m "prerelease: generate changelog, bump version to v$version_to_bump"
+git add . && git commit -m "prerelease: bump 'package.json' for v$version_to_bump"
 git push origin main
 
 # Actually package and release
